a2619b16ae1db9bc17202bf1249379ed17990284
Sagara Gunathunga <sagara@sagara.local>
2024-05-24 14:18:53 +1200

    ch2 - sample1 -step3

--- ch2/helloworld/src/main/java/com/bpb/hssb/ch2/helloworld/HelloworldApplication.java
+++ ch2/helloworld/src/main/java/com/bpb/hssb/ch2/helloworld/HelloworldApplication.java
@@ -1,5 +1,6 @@
 package com.bpb.hssb.ch2.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,15 +8,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloworldApplication implements CommandLineRunner {
 
+	private GreetingService greetingService;
+
+	@Autowired
+	public HelloworldApplication(GreetingService greetingService) {
+		this.greetingService = greetingService;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(HelloworldApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello World");
+		String message = "Hello World, " + greetingService.greet();
+		System.out.println(message);
 	}
-
 }
-
-

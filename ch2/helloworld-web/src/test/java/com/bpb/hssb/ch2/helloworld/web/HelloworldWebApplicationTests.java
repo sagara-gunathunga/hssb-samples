66c9790b539e5e67a4d4ae67edbfe38bbb2ffd3c
Sagara Gunathunga <sagara@sagara.local>
2024-05-24 20:44:39 +1200

    ch2 - sample3 -step1

---
+++ ch2/helloworld-web/src/test/java/com/bpb/hssb/ch2/helloworld/web/HelloworldWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bpb.hssb.ch2.helloworld.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
